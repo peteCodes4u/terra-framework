11457e42375bc99148104d11413804162a82e205
Joseph Ruff <joegruff16@gmail.com>
2024-11-27 21:16:11 -0500

    Deleted all Pete's OG files.

--- client/src/pages/Page1.jsx
+++ client/src/pages/Page1.jsx
@@ -1,30 +1,29 @@
 // react components
 // import { useState, useEffect } from 'react';
 
-// Bootsrap Components
+// Bootstrap Components
 import {
   Container,
-//   add additional bootstrap components here when ready
-//   Col,
-//   Form,
-//   Button,
-//   Card,
-//   Row
-} from 'react-bootstrap';
+  //   add additional bootstrap components here when ready
+  //   Col,
+  //   Form,
+  //   Button,
+  //   Card,
+  //   Row
+} from "react-bootstrap";
 
 const Page1 = () => {
-
-    return (
-        <>
-            <main className='p-2'>
-                <Container>
-                    <center>
-                        <h1 className='py-2'>Terra Framework </h1>
-                    </center>
-                </Container>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main className="p-2">
+        <Container>
+          <center>
+            <h1 className="py-2">Terra Framework </h1>
+          </center>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default Page1;
