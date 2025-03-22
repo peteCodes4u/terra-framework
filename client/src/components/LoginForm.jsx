1ab13def756185e3f569c2c736beca0e429e91c2
joegruff16 <joegruff16@gmail.com>
2025-03-22 18:27:38 -0400

    Created logic using useState to add a drop down menu when a user logs in that displays their profile link and the ability to log out within the login user form.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -12,6 +12,10 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const navigate = useNavigate();
+  // Track login in status for future dropdown menu to be displayed after user logs in
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // Track user ID when they are logged in
+  const [userId, setUserId] = useState(null);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -38,8 +42,10 @@ const LoginForm = () => {
       const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
-      // redirect to profile page
-      navigate(`/profile/${user._id}`);
+
+      // Update login state
+      setIsLoggedIn(true);
+      setUserId(user._id);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -54,52 +60,70 @@ const LoginForm = () => {
 
   return (
     <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert
-          dismissible
-          onClose={() => setShowAlert(false)}
-          show={showAlert}
-          variant="danger"
-        >
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="email">Email</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your email"
-            name="email"
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Email is required!
-          </Form.Control.Feedback>
-        </Form.Group>
+      {/* Add logic to add drop down menu when user is logged in */}
+      {isLoggedIn ? (
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Alert
+            dismissible
+            onClose={() => setShowAlert(false)}
+            show={showAlert}
+            variant="danger"
+          >
+            Something went wrong with your login credentials!
+          </Alert>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your email"
+              name="email"
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Email is required!
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="password">Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Your password"
-            name="password"
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Password is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type="submit"
-          variant="success"
-        >
-          Submit
-        </Button>
-      </Form>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Your password"
+              name="password"
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Password is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            disabled={!(userFormData.email && userFormData.password)}
+            type="submit"
+            variant="success"
+          >
+            Submit
+          </Button>
+        </Form>
+      ) : (
+        // Drop down menu that will display when user is logged in
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Account
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            `
+            <Dropdown.Item onClick={() => navigate(`/profile/${userId}`)}>
+              Your Profile
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => Auth.logout()}>Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
     </>
   );
 };
