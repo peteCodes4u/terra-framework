796a8db667ec40c62b75cbd1597f0b9ae2128385
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-02 15:17:31 -0400

    Component index.jsx directories added

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,4 @@
+// this file is used to create the header component
+export default function appHeader() {
+
+}
---
+++ client/src/components/NavigationBar/index.jsx
@@ -0,0 +1,7 @@
+// this file is used to create the navigation bar component
+import { Link, useLocation } from 'react-router-dom';
+
+// this is the navigation bar component
+export default function NavigationBar() { 
+
+};
