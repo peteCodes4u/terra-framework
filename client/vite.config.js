cc44381894ec5f86838ee68bca73c20b1888da10
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-15 18:58:11 -0400

    Update to specify port in env file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import react from '@vitejs/plugin-react'
+
+const vPort = parseInt(process.env.VITE_PORT);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: vPort,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: `http://localhost:${vPort + 1}`,
         secure: false,
         changeOrigin: true
       }
