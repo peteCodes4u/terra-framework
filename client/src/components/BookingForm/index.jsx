09dd02c53e80c479aa816a08ca07febfb7f04d1c
joegruff16 <joegruff16@gmail.com>
2025-05-16 19:06:35 -0400

    Added a booking page and bookingform component

---
+++ client/src/components/BookingForm/index.jsx
@@ -0,0 +1,22 @@
+export default function BookingForm() {
+  return (
+    <div className="booking-form">
+      <h2>Booking Form</h2>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="date">Date:</label>
+        <input type="date" id="date" name="date" required />
+
+        <label htmlFor="time">Time:</label>
+        <input type="time" id="time" name="time" required />
+
+        <button type="submit">Book Now</button>
+      </form>
+    </div>
+  );
+}
