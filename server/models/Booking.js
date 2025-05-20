9de1d1f2fa80d6a5aecac5a31a475043710afee3
joegruff16 <joegruff16@gmail.com>
2025-05-19 21:55:36 -0400

    Completed Booking model.

---
+++ server/models/Booking.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+// Model will have name, email, date, and time fields
+
+const bookingSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+  // set to use virtuals if needed
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const Booking = model("Booking", bookingSchema);
+module.exports = Booking;
