70c2fcbe89bc242cb628d8bc6998f8f3cd4f8364
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-20 11:16:58 -0400

    Updated header component to include nested NavigationBar

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,4 +1,12 @@
 // this file is used to create the header component
-export default function appHeader() {
+// import the navigation bar compnent to be used in the header
+import NavigationBar from '../NavigationBar';
 
-}
+export default function Header({ toggleStylesheet }) {
+
+    return (
+        <header>
+            <NavigationBar  toggleStylesheet={toggleStylesheet}/>
+        </header>
+    );
+};
