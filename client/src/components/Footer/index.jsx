0f77eadaa6f5a7c9a0303dcf29eb180c1a36d980
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-21 13:59:46 -0400

    first pass at footer update

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
 import {
   Container,
   Navbar,
@@ -10,50 +8,48 @@ import {
   Col,
   Button,
 } from "react-bootstrap";
-// React icons that display social media icons and links
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
-import React from "react";
-
-const AppFooter = () => {
+export default function AppFooter() {
   return (
-    <Navbar bg="dark" variant="dark" className="mt-4">
-      <Container fluid>
-        <Row className="w-100 align-items-center">
-          <Col xs={12} md={4} className="mb-3 mb-md-0">
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="mr-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Col>
-          <Col xs={12} md={4} className="text-center text-md-right text-light">
-            <div className="mb-2">
-              <Nav className="d-flex justify-content-center justify-content-md-end">
-                <Nav.Link href="https://facebook.com" target="_blank">
-                  <FaFacebook />
-                </Nav.Link>
-                <Nav.Link href="https://twitter.com" target="_blank">
-                  <FaTwitter />
-                </Nav.Link>
-                <Nav.Link href="https://instagram.com" target="_blank">
-                  <FaInstagram />
-                </Nav.Link>
-              </Nav>
-            </div>
-            <div>
-              &copy; {new Date().getFullYear()} Terra API Framework. All rights
-              reserved.
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </Navbar>
+    <footer className="app-style1-footer">
+      <Navbar className="footer-navbar">
+        <Container fluid>
+          <Row className="w-100 align-items-center">
+            <Col xs={12} md={4}>
+              <Form className="d-flex footer-search">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <Button type="submit" className="footer-search-btn">
+                  Search
+                </Button>
+              </Form>
+            </Col>
+            <Col xs={12} md={4} className="footer-social-col">
+              <div className="footer-social">
+                <Nav className="d-flex justify-content-center justify-content-md-end">
+                  <Nav.Link href="https://facebook.com" target="_blank">
+                    <FaFacebook />
+                  </Nav.Link>
+                  <Nav.Link href="https://twitter.com" target="_blank">
+                    <FaTwitter />
+                  </Nav.Link>
+                  <Nav.Link href="https://instagram.com" target="_blank">
+                    <FaInstagram />
+                  </Nav.Link>
+                </Nav>
+              </div>
+              <div className="footer-copyright">
+                &copy; {new Date().getFullYear()} Terra API Framework. All rights
+                reserved.
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Navbar>
+    </footer>
   );
-};
-
-export default AppFooter;
+}
