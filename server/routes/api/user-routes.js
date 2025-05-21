41898083af5d257aaf0e2817a3524ffe477e505b
joegruff16 <joegruff16@gmail.com>
2025-05-21 19:04:05 -0400

    Disabled Prettier extension because it was adding double quotes to everything which could cause bugs. Updated anything in doubles to singles in the user-routes

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -8,16 +8,14 @@ const {
 } = require('../../controllers/user-controller');
 
 // import needed middleware from utils/auth
-const { authMiddleware } = require("../../utils/auth");
+const { authMiddleware } = require('../../utils/auth');
 
 // update (METHOD = put) authMiddleware to send a token for verification on API requests
-router.route("/").post(createUser).put(authMiddleware);
+router.route('/').post(createUser).put(authMiddleware);
 
-router.route("/login").post(login);
+router.route('/login').post(login);
 
-router.route("/me").get(authMiddleware, getSingleUser);
-
-
+router.route('/me').get(authMiddleware, getSingleUser);
 
 // update (METHOD = put) authMiddleware to send a token for verification on API requests
 // delete (METHOD = delete) authMiddleware to send a token for verification on API requests
