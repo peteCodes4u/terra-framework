6496fe82392ec8c02aadd7d3ae2924c72b87c5e3
joegruff16 <joegruff16@gmail.com>
2025-05-22 13:35:50 -0400

    Created a API route to get all bookings after a booking is created.

--- server/routes/api/booking-routes.js
+++ server/routes/api/booking-routes.js
@@ -1,10 +1,17 @@
-// const router = require("express").Router();
-// const {
-//   createBooking,
-//   getAllBookings,
-//   updateBooking,
-//   deleteBooking,
-// } = require("../../controllers/booking-controller");
+const router = require('express').Router();
+const {
+    createBooking,
+    getAllBookings,
+    updateBooking,
+    deleteBooking,
+} = require("../../controllers/booking-controller");
 
-// // import needed middleware from utils/auth
-// const { authMiddleware } = require("../../utils/auth");
+// import needed middleware from utils/auth
+const { authMiddleware } = require("../../utils/auth");
+
+// When booking is created, GET all bookings
+router.route('/').post(authMiddleware, createBooking).get(authMiddleware, getAllBookings);
+// When booking is updated, PUT to update the booking
+
+
+module.exports = router;
