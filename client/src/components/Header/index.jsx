f95329382872e00a4aea74cd746b640b75190d91
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-22 14:57:30 -0400

    Updated theme toggle button to dropdown

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -2,11 +2,15 @@
 // import the navigation bar compnent to be used in the header
 import NavigationBar from '../NavigationBar';
 
-export default function Header({ toggleStylesheet }) {
+export default function Header({ toggleStylesheet, isStyle1Active, setIsStyle1Active }) {
 
     return (
         <header className="app-style1-header">
-            <NavigationBar  toggleStylesheet={toggleStylesheet}/>
+            <NavigationBar
+                toggleStylesheet={toggleStylesheet}
+                isStyle1Active={isStyle1Active}
+                setIsStyle1Active={setIsStyle1Active}
+            />
         </header>
     );
 };
