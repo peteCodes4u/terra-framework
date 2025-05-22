f343ed9dd3aff7c233a3eef688a58488435a938a
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-22 19:07:55 -0400

    completed implementation of dynmaic class names for Signup form

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,12 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import SignupForm from "../components/SignUpForm";
+import { useStyle } from "../StyleContext";
 
 export default function Signup() {
+  const { activeStyle } = useStyle();
   const navigate = useNavigate();
   return (
     <div className="custom-popup-overlay">
       <div className="custom-popup-window">
-        <SignupForm handleModalClose={() => navigate("/")} />
+        <SignupForm
+          handleModalClose={() => navigate("/")}
+          activeStyle={activeStyle}
+        />
       </div>
     </div>
   );
