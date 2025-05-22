6c595c624382392f48e61ba61aae06a5e471ac20
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-22 19:00:14 -0400

    completed integration of dynamic className solution to loginForm component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useStyle } from "../StyleContext";
 import LoginForm from "../components/LoginForm";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const { activeStyle } = useStyle();
   const navigate = useNavigate();
   return (
     <div className="custom-popup-overlay">
       <div className="custom-popup-window">
-        <LoginForm handleModalClose={() => navigate("/")} />
+        <LoginForm
+          activeStyle={activeStyle}
+          handleModalClose={() => navigate("/")}
+        />
       </div>
     </div>
   );
