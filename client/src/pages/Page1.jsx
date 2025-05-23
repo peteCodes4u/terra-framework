89d9ae04469b9d27a0f81374798f482803b0228a
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-23 12:00:26 -0400

    Updated Page1.jsx to leverage dynamic classNaming

--- client/src/pages/Page1.jsx
+++ client/src/pages/Page1.jsx
@@ -1,5 +1,9 @@
 // outlet
 import { Outlet } from "react-router-dom";
+// Styling context used for importing context to use the active style. Context is a way to
+// share data between components without having to pass props down manually at every level.
+// This is useful for theming, as it allows us to change the style of the entire app
+import { useStyle } from "../StyleContext";
 
 // Bootstrap Components
 import {
@@ -10,53 +14,49 @@ import {
 } from "react-bootstrap";
 
 const Page1 = () => {
-  // return (
-  // );
-
+  const { activeStyle } = useStyle();
   return (
-    <div>
-      <>
-        <main className="py-2">
-          <Container className="text-center">
-            <h1 className="py-2">Terra Framework </h1>
-            <Row className="py-4">
-              <Col md={4}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>Forms</Card.Title>
-                    <Card.Text>
-                      This image or gif could be clickable to link to a new page
-                      that goes into detail about how to leverage this solution
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col md={4}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>User Interface</Card.Title>
-                    <Card.Text>
-                      This could be an image of the code and an image or gif of
-                      the actual solution
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col md={4}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>Database solutions</Card.Title>
-                    <Card.Text>
-                      We could also include links in the navbar to these solutions
-                      as well
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </main>
-      </>
+    <div className={`${activeStyle}-page1`}>
+      <main className="py-2">
+        <Container className="text-center">
+          <h1 className="py-2">Terra Framework </h1>
+          <Row className="py-4">
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Forms</Card.Title>
+                  <Card.Text>
+                    This image or gif could be clickable to link to a new page
+                    that goes into detail about how to leverage this solution
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>User Interface</Card.Title>
+                  <Card.Text>
+                    This could be an image of the code and an image or gif of
+                    the actual solution
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Database solutions</Card.Title>
+                  <Card.Text>
+                    We could also include links in the navbar to these solutions
+                    as well
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </main>
       <Outlet />
     </div>
   );
