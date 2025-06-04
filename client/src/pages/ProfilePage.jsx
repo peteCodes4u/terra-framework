73fa66c1668a4f76115f10ee40309ffaf6e32645
joegruff16 <joegruff16@gmail.com>
2025-06-04 17:15:36 -0400

    Through importing the booking form in the profile page I was able to get it to display.

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,15 +1,20 @@
 // Import necessary packages
 import { useParams } from "react-router-dom";
 import { useStyle } from "../StyleContext";
+import BookingForm from "../components/BookingForm";
 
-export default function ProfilePage({ toggleStylesheet }) {
+export default function ProfilePage({ toggleStylesheet}) {
   const { id } = useParams();
   const { activeStyle } = useStyle();
 
   return (
+  
     <div className={`${activeStyle}-profile-page`}>
       <h1>🛸 Welcome to your Profile Page! 🛸</h1>
       <p>your userId is: {id}</p>
-    </div>
+      <BookingForm />
+      <p>Feel free to book a consultation with us!</p>
+      </div>
+    
   );
 };
