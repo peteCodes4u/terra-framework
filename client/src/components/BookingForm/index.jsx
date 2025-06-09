e0f60eefc438c1b8901e1fec0276e077e5373d64
joegruff16 <joegruff16@gmail.com>
2025-06-09 19:00:57 -0400

    I forgot to include logic within the BookingForm component that is creating a POST request when book now is selected as there was no onSubmit handler peresent so the browser would basically reload the page and no request to add a booking would be made.

--- client/src/components/BookingForm/index.jsx
+++ client/src/components/BookingForm/index.jsx
@@ -1,22 +1,58 @@
+import { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+// import { createBooking } from '../../utils/API';
+// import Auth from '../../utils/auth';
+
+
 export default function BookingForm() {
+  // set form state
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    date: '',
+    time: ''
+  });
+  // set state for form validation
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  // handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('/api/bookings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      alert('Booking Created Successfully!');
+    } else {
+      alert('Failed to create booking. Please try again.');
+    }
+  };
+
   return (
     <div className="booking-form">
       <h2>Booking</h2>
-      <form>
+      <Form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
+        <input type="text" id="name" name="name" required onChange={handleChange} />
 
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" required onChange={handleChange} />
 
         <label htmlFor="date">Date:</label>
-        <input type="date" id="date" name="date" required />
+        <input type="date" id="date" name="date" required onChange={handleChange} />
 
         <label htmlFor="time">Time:</label>
-        <input type="time" id="time" name="time" required />
+        <input type="time" id="time" name="time" required onChange={handleChange} />
 
-        <button type="submit">Book Now</button>
-      </form>
+        <Button type="submit">Book Now</Button>
+      </Form>
     </div>
   );
 }
