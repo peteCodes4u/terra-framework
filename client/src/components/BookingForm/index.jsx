6f712568d9ca4a6e8b04dd56cd9579b4275597da
joegruff16 <joegruff16@gmail.com>
2025-06-11 21:29:04 -0400

    Added validation for error handling in the booking form to try to figure out why a booking isn't being created after I try to book a consult.

--- client/src/components/BookingForm/index.jsx
+++ client/src/components/BookingForm/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createBooking } from '../../utils/API';
+// import { createBooking } from '../../utils/API';
 import Auth from '../../utils/auth';
 
 
