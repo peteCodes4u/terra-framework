454fad7b58388e50b493034e64e4e449b5e3328b
joegruff16 <joegruff16@gmail.com>
2025-07-02 22:16:28 -0400

    Updated the getAllBookings function in the booking controller to ensure that when a user books a consultation only that particular users bookings will be retrieved when logged in.

--- server/controllers/booking-controller.js
+++ server/controllers/booking-controller.js
@@ -19,42 +19,18 @@ module.exports = {
     }
 
 
-  }
+  },
   // Get all bookings
-  async getAllBookings(context) {
-
+  async getAllBookings(req, res) {
     try {
-      const bookings = await Booking.find({});
-      if (!bookings || bookings.length === 0) {
-        return res.status(404).json({ message: "No bookings found" });
-      }
+      const bookings = await Booking.find({ user: req.user._id });
       res.json(bookings);
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Internal server error" });
     }
-    // const token = signToken(bookings);
-    // res.json({ token, bookings });
-  },
-  // Get user by id or email
-  // async getOneBooking({ name = null, params }, res) {
-  //   try {
-  //     const foundBooking = await Booking.findOne({
-  //       $or: [
-  //         { _id: name ? name._id : params.id }
 
-  //       ],
-  //     });
-  //     if (!foundBooking) {
-  //       return res.status(400).json({
-  //         message: "Sorry this booking doesn't exist in our records",
-  //       });
-  //     }
-  //     res.json(foundBooking);
-  //   } catch (err) {
-  //     res.status(500).json({ message: "internal server error" });
-  //   }
-  // },
+  },
   //   Delete a booking
   async deleteBooking(req, res) {
     try {
