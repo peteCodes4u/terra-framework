df34d2e2646683171a324b3cde899d6f8a5b5253
joegruff16 <joegruff16@gmail.com>
2026-05-11 20:44:38 -0400

    Added import of userTimeZoneMiddleware to apply timezone to availability routes within availibility-routes api.

--- server/routes/api/availability-routes.js
+++ server/routes/api/availability-routes.js
@@ -1,7 +1,9 @@
+// Apply Timezone to availability routes
+import { userTimeZoneMiddleware } from "../../middleware/user-timezone";
 const express = require("express");
 const router = express.Router();
 const { checkAvailability } = require('../../controllers/availability-controller');
 
-router.get("/", checkAvailability);
+router.get("/", userTimeZoneMiddleware, checkAvailability);
 
 module.exports = router;
