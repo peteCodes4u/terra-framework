331c20807db087d5e5075fd1644425309246a0df
joegruff16 <joegruff16@gmail.com>
2026-05-22 17:43:00 -0400

    replaced the existing localstorage save line with user_tz so that we aren't duplicating key names.

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -32,7 +32,7 @@ const LoginForm = ({ handleModalClose, activeStyle }) => {
       }
       // Store user time zone in localStorage for later use
       const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone || 'UTC';
-      localStorage.setItem('userTimeZone', userTimeZone);
+      localStorage.setItem('user_tz', userTimeZone);
 
       const { token, user } = await response.json();
       Auth.login(token);
