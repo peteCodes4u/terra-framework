bce0e2298efd97b2238f3fb3b96fc4e91dbf1592
joegruff16 <joegruff16@gmail.com>
2026-06-15 19:59:07 -0400

    Removed the import to create code coonsistency since I am using a const to import checkAvailability.

--- server/routes/api/availability-routes.js
+++ server/routes/api/availability-routes.js
@@ -1,5 +1,5 @@
 // Apply Timezone to availability routes
-import { userTimeZoneMiddleware } from "../../middleware/user-timezone";
+const { userTimeZoneMiddleware } = require("../../middleware/user-timezone");
 const express = require("express");
 const router = express.Router();
 const { checkAvailability } = require('../../controllers/availability-controller');
